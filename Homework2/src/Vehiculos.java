9a02ad31d16b4cc419274c22c96c1d29eaa08b79
David Basantes <sebastian2405lucero@hotmail.com>
2022-11-23 22:52:55 -0500

    Cambio del deber con JOptionPane

--- Homework2/src/Vehiculos.java
+++ Homework2/src/Vehiculos.java
@@ -1,3 +1,5 @@
+//Libaray
+import javax.swing.JOptionPane;
 public class Vehiculos {
 
     //ATTRIBUTES
@@ -10,6 +12,8 @@ public class Vehiculos {
     String Modelo;
     int Ruedas_Elices_Aletas_Vehiculo;
     
+    //Array for the cars 
+    Vehiculos vehiculo[] = new Vehiculos[5];
 
     //METHODS
     public  void encender_vehiculo(){
@@ -26,11 +30,6 @@ public class Vehiculos {
     }
 
     //CONSTRUCTOR
-    
-    //GETTER AND SETTER
-    public String getPlaca() {
-        return Placa;
-    }
     public Vehiculos(String placa, String tipo_De_Vehiculo, String motor_De_Vehiculo, String color, String marca,
             int precio, String modelo, int ruedas_Elices_Aletas_Vehiculo) {
         Placa = placa;
@@ -42,6 +41,11 @@ public class Vehiculos {
         Modelo = modelo;
         Ruedas_Elices_Aletas_Vehiculo = ruedas_Elices_Aletas_Vehiculo;
     }
+    //GETTER AND SETTER
+    public String getPlaca() {
+        return Placa;
+    }
+   
     public void setPlaca(String placa) {
         Placa = placa;
     }
@@ -92,20 +96,39 @@ public class Vehiculos {
     public void setModelo(String modelo) {
         Modelo = modelo;
     }
+
     
+    @Override
+    public String toString() {
+        return "Vehiculos Placa=" + Placa + "\n Tipo_De_Vehiculo=" + Tipo_De_Vehiculo + "\nMotor_De_Vehiculo="
+                + Motor_De_Vehiculo + "\nColor=" + Color + "\nMarca=" + Marca + "\nPrecio=" + Precio + "\nModelo="
+                + Modelo + "\nRuedas_Elices_Aletas_Vehiculo=" + Ruedas_Elices_Aletas_Vehiculo;
+
+    }
+
     //Funtion for menu 
 
 
+    
     public void Ver_Vehiculos(){
-        System.out.println("");
-        System.out.println("Marca " + getMarca());
-        System.out.println("Modelo " + getModelo());
-        System.out.println("Precio " + getPrecio());
-        System.out.println("Color " + getColor());
-        System.out.println("Tipo de vehiculo " + getTipo_De_Vehiculo());
-        System.out.println("Motor del vehiculo " + getMotor_De_Vehiculo());
-        System.out.println("Rudas Elices o Aletas  " + getRudas_Elices_Aletas_Vehiculo());
-        System.out.println("");
+        for(Vehiculos vehiculos: vehiculo){
+            JOptionPane.showMessageDialog(null, "Los 5 vehiculos ingresados son\n"+ vehiculos.toString());;
+        }
+    }
+
+    public void Llenar_Vehiculos(){
+        for(int i = 0; i < vehiculo.length; i++){
+            Placa = JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese la placa de su Vehiculo" );
+            Marca = JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese la marca de su Vehiculo");
+            Modelo = JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese el modelo de su Vehiculo");
+            Color = JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese el color de su Vehiculo");
+            Tipo_De_Vehiculo = JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese el tipo de Vehiculo");
+            Motor_De_Vehiculo = JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese el tipo de motor de su Vehiculo");
+            Precio = Integer.parseInt(JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese el precio de su Vehiculo"));
+            Ruedas_Elices_Aletas_Vehiculo = Integer.parseInt(JOptionPane.showInputDialog("Vehiculo Nº" + (i + 1) +"\nIngrese el numero de ruedas, aletas, alas de su Vehiculo"));
+            vehiculo[i] = new Vehiculos(Placa, Tipo_De_Vehiculo, Motor_De_Vehiculo, Color, Marca, Precio,Modelo, Ruedas_Elices_Aletas_Vehiculo);
+        }
+
     }
    
     
